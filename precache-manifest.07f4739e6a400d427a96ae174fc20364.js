a0fda17f330e8b83c45b2b34046a3f4fd0a94e78
Claudiaj501 <claudiaj.501@outlook.com>
2022-11-06 19:22:48 +0100

    Updates

---
+++ precache-manifest.07f4739e6a400d427a96ae174fc20364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d35ed9d8f01ac2b2ffc4",
+    "url": "/meet/static/css/main.a5dbcdb8.chunk.css"
+  },
+  {
+    "revision": "d35ed9d8f01ac2b2ffc4",
+    "url": "/meet/static/js/main.4dab201d.chunk.js"
+  },
+  {
+    "revision": "1b9c9f39f8be985be7b7",
+    "url": "/meet/static/js/runtime~main.454a05bf.js"
+  },
+  {
+    "revision": "53c7029cd391f74539b7",
+    "url": "/meet/static/js/2.5d65d9fa.chunk.js"
+  },
+  {
+    "revision": "0d238a3a755f2811deea",
+    "url": "/meet/static/js/3.d90efc57.chunk.js"
+  },
+  {
+    "revision": "2059dc01bc414a160218dfa23ec721e8",
+    "url": "/meet/index.html"
+  }
+];
