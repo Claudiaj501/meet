c5b26ebd70a9954ded348273ea6115efb028fdd3
Claudiaj501 <claudiaj.501@outlook.com>
2022-11-18 23:11:29 +0100

    Updates

---
+++ precache-manifest.b3447fbf53cb44b9d501a97f840f8fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "180d0617354c959ac732",
+    "url": "/meet/static/css/main.47ce7f01.chunk.css"
+  },
+  {
+    "revision": "180d0617354c959ac732",
+    "url": "/meet/static/js/main.f6407552.chunk.js"
+  },
+  {
+    "revision": "dd4ea85d0d3f10afc010",
+    "url": "/meet/static/js/runtime~main.e38513b7.js"
+  },
+  {
+    "revision": "c68d3bd0bc1d2ef870da",
+    "url": "/meet/static/js/2.0a1b7a86.chunk.js"
+  },
+  {
+    "revision": "5b4d7cbf33e1065134f9",
+    "url": "/meet/static/js/3.7ff166bd.chunk.js"
+  },
+  {
+    "revision": "dc53aee91cbd19f5b22bbab05b66a0c3",
+    "url": "/meet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.466b6e6653a14c9f5b4554e9018d61c4.js"
+  "/meet/precache-manifest.b3447fbf53cb44b9d501a97f840f8fd0.js"
 );
 
 workbox.clientsClaim();
