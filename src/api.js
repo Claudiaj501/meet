9612792d7152e5b0bdccaa46fcb33c11e8e5d9c4
Claudiaj501 <claudiaj.501@outlook.com>
2022-11-18 19:54:27 +0100

    online and offline storage

--- src/api.js
+++ src/api.js
@@ -71,6 +71,12 @@ if (isLocal) {
   return mockData;
 }
 
+if (!navigator.onLine) {
+  const data = localStorage.getItem("lastEvents");
+  NProgress.done();
+  return data?JSON.parse(events).events:[];;
+}
+
   const token = await getAccessToken();
 
   if (token) {
